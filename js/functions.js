e7163601fb76af322b17c243f3544967c4b03a88
Blyscop <Blyscop@MacBook-Pro-de-Blyscop.local>
2016-05-25 17:02:07 +0200

    refactor index.js

--- js/functions.js
+++ js/functions.js
@@ -10,19 +10,19 @@ function getPreferencesTypes(type)
     if(type=="drink")
     {
         action="get_drinks";
-        divID="#drinkPreferences"; 
+        divID="#drinkPreferences";
     }
     if(type=="food")
     {
         action="get_foods";
-        divID="#foodPreferences"; 
+        divID="#foodPreferences";
     }
-    $.ajax({ 
-        type: 'GET', 
-        url: 'http://localhost/Tweevent_Session2/tweevent_front-master/test.php?action='+action, 
-        data: { get_param: 'libelle' }, 
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost/Tweevent_Session2/tweevent_front-master/test.php?action='+action,
+        data: { get_param: 'libelle' },
         dataType: 'json',
-        success: function (data) { 
+        success: function (data) {
             $.each(data, function(index, element) {
                 console.log(element);
                 $(divID).append("<div><input type='checkbox'/> "+element.libelle+"</div>");
@@ -30,6 +30,44 @@ function getPreferencesTypes(type)
         }
     });
     console.log('http://localhost/Tweevent_Session2/tweevent_front-master/test.php?action='+action);
-}  
+}
 
+function inscription()
+{
+    var _pseudo=$('#pseudo').val();
+    var _password=$('#password').val();
+    var _action="Utilisateur_ADD";
+    var _chk="enregistrer";
 
+    $.ajax({
+        type: "POST",
+        url: "http://martinfrouin.fr/projets/tweevent/q/req.php",
+        data: {pseudo:_pseudo,password:_password,action:_action,chk:_chk},
+        success: function(msg) {
+            var msg = $.parseJSON(msg);
+            if(msg.utilisateur!=null) {
+                console.log(msg.utilisateur);
+                connect(_pseudo,_password)
+            }
+            else
+            {
+                $('#info_inscription').empty();
+                $('#info_inscription').append('<div class="form-group col-sm-12"><label for="pseudo" class="col-sm-3 control-label">Information : </label><div class="col-sm-6"><p style="color:red;">'+msg.message+'</p></div></div>');
+            }
+
+        }
+    });
+}
+
+function connect(_username,_pwd)
+{
+    $.ajax({
+        type: "POST",
+        url: "session.php",
+        data: {username:_username,password:_pwd},
+        success: function(msg) {
+            console.log(msg);
+            document.location.replace("Actualite.php");
+        }
+    });
+}
