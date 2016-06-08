38b26b5d3b580a469b178d0c9bdb94455fa0461b
flofloooooooooooooo <florian.vassiliadi@epsi.fr>
2016-06-08 17:42:11 +0200

    ajout menu gauche à la page profil

--- Profil.php
+++ Profil.php
@@ -89,65 +89,8 @@ check_session(); ?>
 <div class="wrapper">
     <div class="box">
         <div class="row row-offcanvas row-offcanvas-left">
-            <!-- sidebar -->
-            <div class="column col-sm-2 col-xs-1 sidebar-offcanvas" id="sidebar">
-                <ul class="nav">
-                    <li>
-                        <a href="#" data-toggle="offcanvas"
-                           class="visible-xs text-center">
-                            <i class="glyphicon glyphicon-chevron-right"></i>
-                        </a>
-                    </li>
-                </ul>
-                <ul class="nav hidden-xs" id="lg-menu">
-                    <li>
-                        <a href="#" role="button"
-                           data-toggle="modal">
-                            <i class="glyphicon glyphicon-list-alt"></i>
-                            Préferences
-                        </a>
-                    </li>
-                    <li>
-                        <a href="Calendrier">
-                            <i class="glyphicon glyphicon-list"></i>
-                            Calendrier
-                        </a>
-                    </li>
-                    <li>
-                        <a id="addEvent" href="#" data-toggle="modal" data-target=".addEvent">
-                            <i class="glyphicon glyphicon-paperclip"></i>
-                            Ajouter Evenement
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="glyphicon glyphicon-refresh"></i>
-                            Refresh
-                        </a>
-                    </li>
-                </ul>
-                <ul class="list-unstyled hidden-xs" id="sidebar-footer">
-                    <li>
-                        <a href="#">
-                            <h3>Tweevent</h3>
-                            <i class="glyphicon glyphicon-heart-empty"></i>
-                            Events
-                        </a>
-                    </li>
-                </ul>
-
-                <!-- tiny only nav-->
-                <ul class="nav visible-xs" id="xs-menu">
-                    <li>
-                        <a href="#" role="button"
-                           data-toggle="modal">
-                            <i class="glyphicon glyphicon-list-alt"></i>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-            <!-- /sidebar -->
+           <!--            INCLURE LE NAVBAR_LEFT ICI-->
+            <? include("navbar_left.php"); ?>
             <!-- main right col -->
             <div class="column col-sm-10 col-xs-11" id="main">
                 <!-- top nav -->
