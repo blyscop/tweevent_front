5602c1e9c633d4d0ab2967cb76bb3696df0e6667
flofloooooooooooooo <florian.vassiliadi@epsi.fr>
2016-06-08 17:21:53 +0200

    On met le menu de gauche dans navbar_left.php

--- Actualite.php
+++ Actualite.php
@@ -218,69 +218,8 @@ check_session(); ?>
     <div class="wrapper">
         <div class="box">
             <div class="row row-offcanvas row-offcanvas-left">
-                <!-- sidebar -->
-                <div class="column col-sm-2 col-xs-1 sidebar-offcanvas" id="sidebar">
-                    <ul class="nav">
-                        <li>
-                            <a href="#" data-toggle="offcanvas"
-                            class="visible-xs text-center">
-                            <i class="glyphicon glyphicon-chevron-right"></i>
-                        </a>
-                    </li>
-                </ul>
-                <ul class="nav hidden-xs" id="lg-menu">
-                    <li>
-                        <a href="#preferenceModal" role="button"
-                        data-toggle="modal">
-                        <i class="glyphicon glyphicon-list-alt"></i>
-                        Préferences
-                    </a>
-                </li>
-                <li>
-                    <a href="Calendrier">
-                        <i class="glyphicon glyphicon-list"></i>
-                        Calendrier
-                    </a>
-                </li>
-                <?php if($_COOKIE['utilisateur_type']=="pro"){?>
-                <li>
-                    <a id="addEvent" href="#" data-toggle="modal" data-target=".addEvent">
-                        <i class="glyphicon glyphicon-paperclip"></i>
-                        Ajouter Evenement
-                    </a>
-                </li>
-                <?php } ?>
-                    
-                <li>
-                    <a href="#">
-                        <i class="glyphicon glyphicon-refresh"></i>
-                        Refresh
-                    </a>
-                </li>
-            </ul>
-            <ul class="list-unstyled hidden-xs" id="sidebar-footer">
-                <li>
-                    <a href="#">
-                        <h3>Tweevent</h3>
-                        <i class="glyphicon glyphicon-heart-empty"></i>
-                        Events
-                    </a>
-                </li>
-            </ul>
-
-            <!-- tiny only nav-->
-            <ul class="nav visible-xs" id="xs-menu">
-                <li>
-                    <a href="#preferenceModal" class="text-center" role="button" data-toggle="modal">
-                        <i class="glyphicon glyphicon-list-alt"></i>
-                    </a>
-                </li>
-                <li><a href="Calendrier" class="text-center"><i class="glyphicon glyphicon-list"></i></a></li>
-                <li><a id="addEvent" href="#" class="text-center" data-toggle="modal" data-target=".addEvent"><i class="glyphicon glyphicon-paperclip"></i></a></li>
-                <li><a href="#" class="text-center"><i class="glyphicon glyphicon-refresh"></i></a></li>
-            </ul>
-        </div>
-        <!-- /sidebar -->
+<!--            INCLURE LE NAVBAR_LEFT ICI-->
+            <? include("navbar_left.php"); ?>
         <!-- main right col -->
         <div class="column col-sm-10 col-xs-11" id="main">
             <!-- top nav -->
