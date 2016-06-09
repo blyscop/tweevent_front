16cdd06c1bc15b725582ef8831f51b902bd59254
Martin Frouin <mart-33@hotmail.fr>
2016-06-09 15:44:53 +0200

    modifs actu

--- header_actu.php
+++ header_actu.php
@@ -1,4 +1,4 @@
 <header>
     <img class="bloc-round" src="./img/profilpic.jpg"/>
-    <h1>Bonjour <?php echo ucfirst($_COOKIE['username']); ?></h1>
+    <h1 class="username_actu">Bonjour <?php echo ucfirst($_COOKIE['username']); ?></h1>
 </header>
